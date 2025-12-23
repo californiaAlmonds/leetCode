268a44f266fe0e834e7d024554f3df32161d6f93
californiaAlmonds <160736368+californiaAlmonds@users.noreply.github.com>
2025-12-23 07:38:48 +0000

    Fix binary search condition and update target in main method; add staircase search method

--- Search2dMatrix.java
+++ Search2dMatrix.java
@@ -11,8 +11,22 @@ public class Search2dMatrix{
             int mid = left + (right-left)/2;
             int row = mid/cols; int col = mid%cols;
             if(matrix[row][col] == target) return true;
-            if(matrix[row][col]<target) right = mid-1;
+            if(matrix[row][col]>target) right = mid-1;
             else left = mid+1; 
+        }
+        return false;
+    }
+
+    public static boolean searchMatrixStairCase(int[][] matrix, int target) {
+        if(matrix.length == 0) return false;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int row = 0;
+        int col = cols-1;
+        while(row<rows && col>=0){
+            if(matrix[row][col] == target) return true;
+            else if(target<matrix[row][col]) col--;
+            else row++;
         }
         return false;
     }
@@ -20,8 +34,9 @@ public class Search2dMatrix{
 
     public static void main(String args[]){
         int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
-        int target = 3;
+        int target = 9;
         System.out.println(searchMatrix(matrix, target));
+        System.out.println(searchMatrixStairCase(matrix, target));
 
 
 
